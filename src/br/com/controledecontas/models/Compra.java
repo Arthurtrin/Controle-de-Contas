0703bce4918ca92e2197b2f726722b26c0bdf24f
Arthurtrin <arthurtrindade08102004@gmail.com>
2025-12-12 11:40:36 -0300

    final commit

--- src/br/com/controledecontas/models/Compra.java
+++ src/br/com/controledecontas/models/Compra.java
@@ -1,4 +1,30 @@
 package br.com.controledecontas.models;
 
-public class Compra {
+public class Compra implements Comparable<Compra>{
+    private String descricao;
+    private double valor;
+
+    public Compra(String descricao, double valor) {
+        this.descricao = descricao;
+        this.valor = valor;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    @Override
+    public String toString() {
+        return "Compra: descrição = " + descricao +
+                " valor = " + valor;
+    }
+
+    @Override
+    public int compareTo(Compra outraCompra) {
+        return Double.valueOf(this.valor).compareTo(Double.valueOf(outraCompra.valor));
+    }
 }
--- src/br/com/controledecontas/models/Conta.java
+++ src/br/com/controledecontas/models/Conta.java
@@ -1,4 +1,37 @@
 package br.com.controledecontas.models;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Conta {
+    private double limite;
+    private double saldo;
+    private List<Compra> compra;
+
+    public Conta(double limite) {
+        this.limite = limite;
+        this.saldo = limite;
+        this.compra = new ArrayList<>();
+    }
+
+    public boolean fazCompra(Compra compra){
+        if(this.saldo>compra.getValor()){
+            this.saldo -= compra.getValor();
+            this.compra.add(compra);
+            return true;
+        }
+        return false;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public List<Compra> getCompras() {
+        return compra;
+    }
+
+    public double getLimite() {
+        return limite;
+    }
 }
